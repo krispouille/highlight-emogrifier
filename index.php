e68ce340946dec7155c0075ad39ffd9663d2c26f
Chrislain Jemba <cjemba@autopassgroup.com>
2019-09-11 05:38:41 +0200

    new layout

--- index.php
+++ index.php
@@ -41,111 +41,87 @@ if (is_xhr()){
     <!--<link rel="stylesheet" href="./styles/default.css" />-->
     <link id="theme-css" rel="stylesheet" href="" />
     <style>
-	#snippet{display:block}
-	pre{width:100%;height:75%}
+    .col{width:50%;height:100%;float:left}
+	#snippet{display:block;background:#eee}
+	pre{width:100%;height:75%;margin:0;padding:0}
 	textarea{width:100%;height:25%}
 
-	#code{height:100%;width:100%}
+	code{height:100%;width:100%}
 	.tab{height:100%;width:100%;display:none}
-
-	#preview{position:absolute;top:0;right:0}
+    #preview{text-align:center}
     </style>
 </head>
 <body>
-    <section>
-	<div id="snippet" class="tab">
-	<pre><code id="code" class="hljs" spellcheck="false"  contenteditable><?=htmlentities('class StepController extends AbstractController {
-	use \Application\Service\ServiceLocatorAwareTrait;
-	use \Euromaster\Quotation\Service\QuoteServiceTrait;
-	use \Euromaster\Quotation\Service\VehicleServiceTrait;')?>
-	</code></pre>
-	<textarea spellcheck="false"></textarea>
-	</div>
-	<div id="output" class="tab">
-	<pre></pre>
-	<textarea spellcheck="false"></textarea>
-	</div>
-    </section>
     <section id="preview">
-            <!--<select id="langs" name="langs"></select>-->
-            <select id="themes" name="themes" data-target="snippet"></select>
-	    <input id="paste-email-code" value="EMAIL" type="button" data-target="output" />
+        <select id="langs" name="langs" data-target="snippet"></select>
+        <select id="themes" name="themes" data-target="snippet"></select>
+        <input id="paste-preview-code" value="PREVIEW" type="button" data-target="snippet" />
+        <input id="paste-email-code" value="EMAIL" type="button" data-target="output" />
     </section>
+    <textarea id="bucket" class="col" spellcheck="false"></textarea>
+    <div class="col">
+        <section>
+	        <div id="snippet" class="tab">
+	            <pre><code id="code" class="hljs" spellcheck="false"></code></pre>
+	            <textarea spellcheck="false"></textarea>
+	        </div>
+	        <div id="output" class="tab">
+	            <pre></pre>
+	            <textarea spellcheck="false"></textarea>
+	        </div>
+        </section>
+    </div>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
     <script src="highlight.pack.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
     <script>
     $(window).load(function(){
         // update langs with hljs.listLanguages()
-	/*var langs = hljs.listLanguages(), slangs=$('#langs');
-	for (var i=0; i<langs.length; i++) {
-	    slangs.append('<option name="'+langs[i]+'">'+langs[i]+'</option>');
-        }*/
+	    var langs = hljs.listLanguages(), slangs=$('#langs');
+        for (var i=0; i<langs.length; i++) {
+            slangs.append('<option name="'+langs[i]+'">'+langs[i]+'</option>');
+        }
 	    var sthemes=$('#themes');
-	// update themes
-	$.post('/', {do: 'get_themes'}, function(data){
-	   themes = $.parseJSON(data);
-	   for (var i=0; i<themes.length; i++) {
-	      sthemes.append('<option name="'+themes[i]+'">'+themes[i]+'</option>');
-	   }
-	});
-	// TODO make sure to hightlight code on focusout
-	//$('body').on('focusout', 'code[contenteditable]',function(){hljs.highlightBlock(document.getElementById('snippet'));});
-	
-	/*$('#langs').on('change', function(e){
-	    $('#snippet code').attr('class', "hljs " + this.value);
-        });*/
+	    // update themes
+	    $.post('/', {do: 'get_themes'}, function(data){
+	        themes = $.parseJSON(data);
+	        for (var i=0; i<themes.length; i++) {
+	            sthemes.append('<option name="'+themes[i]+'">'+themes[i]+'</option>');
+	        }
+	    });
+        // TODO make sure to hightlight code on focusout
+        //$('body').on('focusout', 'code[contenteditable]',function(){hljs.highlightBlock(document.getElementById('snippet'));});
     });
     $('body').on('change click', 'input,select', function(){
-	    var target=$(this).data('target');
+	    var target=$(this).data('target'), theme=$('#themes').val(), lang=$('#langs').val(), raw=$('#bucket').val(), content=hljs.highlight(lang, raw).value;
 	    $('.tab').hide();
 	    $('body').find('#'+target).show();
+        $('#theme-css').attr('href', './styles/'+theme+'.css');
+        $('code').attr('class', "hljs " + lang);
 	    switch (target) {
 	        case 'snippet':
-			$('#theme-css').attr('href', './styles/'+this.value+'.css');
-			var area = $('#snippet textarea'), content=$('#snippet pre').html();
+			var area = $('#snippet textarea'), snippet=$('#snippet pre code');
+			//snippet.html(String(content).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'));
+			snippet.html(content);
 			area.html(content);
 			break;
 	        case 'output':
-			var area = $('#output textarea'), content=$('#snippet pre').html(), output=$('#output pre');
+			var area = $('#output textarea'), output=$('#output pre');
 			$.post('/',{
 				do: 'paste_email_code',
-				code: content,
-				css: [$('#themes').children('option:selected').val()]
+				code: '<code class="hljs '+lang+'" spellcheck=false>'+content+'</code>',
+                //code: String(content).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'),
+				css: [theme]
 			}, function(data) {
 				// update textarea
 				area.html(data);
 				//output.html(String(data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'));
-				output.html($(data));
+				//output.html($(data));
+				output.html(data);
 			});
 			break;
 	    }
     });
-    /*$('#themes').on('change', function(e){
-        $('#theme-css').attr('href', './styles/'+this.value+'.css');
-    });*/
-
-    //$('body').on('focusout', 'code[contenteditable]',function(){hljs.highlightBlock(document.getElementById('snippet'));});
-
-    /*$('#paste-code').on('click', function(e){
-	var area = $('textarea'), content=$('#snippet').html(), output = $('#output');
-	area.html(content);
-    });
-    $('#paste-email-code').on('click', function(e){
-	var area = $('textarea'), content=$('#snippet').html(), output=$('#snippet');
-	$.post('/',{
-	        do: 'paste_email_code',
-	        code: $('#snippet').html(),
-		css: [$('#themes').children('option:selected').val()]
-	    },
-	    function(data){
-		// update textarea
-		area.html(data);
-		//output.html(String(data).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'));
-		output.html($(data));
-	    }
-	);
-    });*/
     </script>
 </body>
 </html>
